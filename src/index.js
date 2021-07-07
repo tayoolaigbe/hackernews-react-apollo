878458474f66aa2210dd6be1c66da3a6b3906fd8
Eyitayo Olaigbe <tayoolaigbe@outlook.com>
2021-07-07 08:54:30 +0100

    Set up the project

---
+++ src/index.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import './styles/index.css';
+import App from './components/App';
+import reportWebVitals from './reportWebVitals';
+import { setContext } from '@apollo/client/link/context';
+import { AUTH_TOKEN } from './constants';
+
+// 1
+import {
+	ApolloProvider,
+	ApolloClient,
+	createHttpLink,
+	InMemoryCache,
+} from '@apollo/client';
+
+// 2
+const httpLink = createHttpLink({
+	uri: 'http://localhost:4000',
+});
+
+const authLink = setContext((_, { headers }) => {
+	const token = localStorage.getItem(AUTH_TOKEN);
+	return {
+		headers: {
+			...headers,
+			authorization: token ? `Bearer ${token}` : '',
+		},
+	};
+});
+
+// 3
+const client = new ApolloClient({
+	link: authLink.concat(httpLink),
+	cache: new InMemoryCache(),
+});
+
+// 4
+ReactDOM.render(
+	<BrowserRouter>
+		<ApolloProvider client={client}>
+			<App />
+		</ApolloProvider>
+	</BrowserRouter>,
+	document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
