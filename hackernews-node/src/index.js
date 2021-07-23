6d98bec44774aa26dcba3276ff29cf9906babdae
Eyitayo Olaigbe <tayoolaigbe@outlook.com>
2021-07-23 10:25:51 +0100

    Successfully added a database

--- hackernews-node/src/index.js
+++ hackernews-node/src/index.js
@@ -1,38 +1,28 @@
 const { ApolloServer } = require('apollo-server');
+const { PrismaClient } = require('@prisma/client');
 const fs = require('fs');
 const path = require('path');
 
-let links = [
-	{
-		id: 'link-0',
-		url: 'www.howtographql.com',
-		description: 'Fullstack tutorial for GraphQL',
-	},
-	{
-		id: 'link-1',
-		url: 'www.laslas.com',
-		description: 'Everybody go de aii las las',
-	},
-];
-
-let idCount = links.length;
+const prisma = new PrismaClient();
 // 2
 const resolvers = {
 	Query: {
 		info: () => `This is the API of a Hackernews Clone`,
-		feed: () => links,
+		feed: async (parent, args, context) => {
+			return context.prisma.link.findMany();
+		},
 	},
 
 	Mutation: {
 		// 2
-		post: (parent, args) => {
-			const link = {
-				id: `link-${idCount++}`,
-				description: args.description,
-				url: args.url,
-			};
-			links.push(link);
-			return link;
+		post: async (parent, args, context) => {
+			const newLink = await context.prisma.link.create({
+				data: {
+					url: args.url,
+					description: args.description,
+				},
+			});
+			return newLink;
 		},
 	},
 };
@@ -41,6 +31,9 @@ const resolvers = {
 const server = new ApolloServer({
 	typeDefs: fs.readFileSync(path.join(__dirname, 'schema.graphql'), 'utf8'),
 	resolvers,
+	context: {
+		prisma,
+	},
 });
 
 server.listen().then(() => {
